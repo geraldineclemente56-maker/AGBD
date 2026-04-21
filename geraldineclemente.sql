511fa8b861deef1df8699d722ed5da4390148f67
geraldineclemente56-maker <geraldineclemente56@gmail.com>
2026-04-21 13:26:47 -0300

    geraldineclemente.sql

--- geraldineclemente.sql
+++ geraldineclemente.sql
@@ -8,20 +8,4 @@ JOIN albums a ON t.AlbumId = a.AlbumId
 WHERE a.Title = 'Big Ones'
 ORDER BY t.Milliseconds DESC;
 
---EJERCICIO 3
-SELECT a.Title, SUM(t.UnitPrice) AS PrecioTotal 
-FROM albums a
-JOIN tracks t ON a.AlbumId = t.AlbumId
-GROUP BY a.AlbumId
-ORDER BY PrecioTotal ASC
-LIMIT 10;
 
---EJERCICIO 4
-SELECT t.Name AS Tema, g.Name AS Genero, a.Title AS Disco
-FROM tracks t
-JOIN genres g ON t.GenreId = g.GenreId
-JOIN albums a ON t.AlbumId = a.AlbumId
-WHERE t.UnitPrice = 0.99
-ORDER BY t.Name ASC;
-
---EJERCICIO 5
