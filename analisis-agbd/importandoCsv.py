e925cd20575c4704ab0fe80981cc2683d8da5a4a
Ayelen Quispe <quispeaguilar.bel3n@gmail.com>
2026-05-26 16:08:55 -0300

    clase 26-05

--- analisis-agbd/importandoCsv.py
+++ analisis-agbd/importandoCsv.py
@@ -5,4 +5,19 @@ import pandas as pd
 #pip3 install pandas
 #Tecnica@UBA
 df=pd.read_csv("2024_medalists_all.csv")
+#Defino una variable en esa variable carga todo mi archivo
+
 print("OKEY! Archivo cargado correctamente")
+#Muestra si carga bien el programa 
+
+# Mostrando las primeras filas del dataframe
+print(df.head())
+
+#resultado = df [df['medalist_name'] == 2024]
+
+#total_medalist =df['medalist_name'].count()
+#esta contando todos los nombres 
+
+total_medalist =df['medalist_name'].sum()
+
+print(f"Total de registros:{total_medalist}")
